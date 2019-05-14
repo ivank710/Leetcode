30107ef24f2f3910b28829190ed904f2ed034bea
Ivan Kwong <ivan.kwong710@gmail.com>
2019-05-14 15:01:48 -0700

    Add O(n) solution to lengthOfLongestSubstr

--- lengthOfLongestSubStr.js
+++ lengthOfLongestSubStr.js
@@ -14,8 +14,25 @@ var lengthOfLongestSubstring = function (s) {
       }
     }
 
-    if (sub.length > max) max = sub.length
+    if (sub.length > max) max = sub.length;
 
+  }
+  return max;
+};
+
+//O(n)
+var lengthOfLongestSubstring = function (s) {
+  if (s.length === 0) return 0;
+  let max = 0; //2
+  let obj = {}; //p:1, w:2, k:3, e:4
+
+  for (let i = 0, j = 0; i < s.length; i++) {
+    if (s[i] in obj) {
+      j = Math.max(j, obj[s[i]] + 1); //j=2
+    }
+
+    obj[s[i]] = i;
+    max = Math.max(max, i - j + 1)
   }
 
   return max;
