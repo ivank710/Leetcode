6a29f6e6ddbdf9947bcea463c3ba7a141a021433
Ivan Kwong <ivan.kwong710@gmail.com>
2019-05-21 10:37:19 -0700

    Add findTilt

---
+++ 563.binary-tree-tilt.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=563 lang=javascript
+ *
+ * [563] Binary Tree Tilt
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const findSum = node => {
+  if (!node) return 0;
+  return node.val + findSum(node.left) + findSum(node.right);
+}
+
+var findTilt = function(root) {
+  if (!root) return 0;
+
+  let left = findSum(root.left);
+  let right = findSum(root.right);
+  let rootTilt = Math.abs(left - right);
+
+  return rootTilt + findTilt(root.left) + findTilt(root.right);
+};
+
