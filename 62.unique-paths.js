409de82dc8e874c52553422d52ac62455b68eb45
Ivan Kwong <ivan.kwong710@gmail.com>
2019-05-30 14:46:22 -0700

    Add uniquePaths

---
+++ 62.unique-paths.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=62 lang=javascript
+ *
+ * [62] Unique Paths
+ */
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+  let grid = Array(m).fill(Array(n).fill(1));
+
+  for (let i = 1; i < grid.length; i++) {
+    for (let j = 1; j < grid[i].length; j++) {
+      grid[i][j] = grid[i][j - 1] + grid[i - 1][j];
+    }
+  }
+
+  return grid[m - 1][n - 1];
+};
+
