f56386c6416ef5c2e0f8bb1931a9a7cb22db37bc
Ivan Kwong <ivan.kwong710@gmail.com>
2019-06-01 20:22:44 -0700

    Add maxDepthTree

---
+++ 104.maximum-depth-of-binary-tree.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=104 lang=javascript
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (!root) return 0;
+
+  let left = maxDepth(root.left) + 1;
+  let right = maxDepth(root.right) + 1;
+
+  return Math.max(left, right) 
+};
+
