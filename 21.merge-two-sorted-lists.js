fd35b7abcd34067064e0ef5926d011f23a0a5deb
Ivan Kwong <ivan.kwong710@gmail.com>
2019-06-01 19:06:27 -0700

    Add mergeTwoSortedLists

--- 21.merge-two-sorted-lists.js
+++ 21.merge-two-sorted-lists.js
@@ -16,6 +16,40 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  let node;
+  let head;
+
+  if (l1.val < l2.val) {
+    head = new ListNode(l1.val);
+    node = head;
+    l1 = l1.next;
+  } else {
+    head = new ListNode(l2.val);
+    node = head;
+    l2 = l2.next;
+  }
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      node.next = new ListNode(l1.val);
+      node = node.next;
+      l1 = l1.next;
+    } else {
+      node.next = new ListNode(l2.val);
+      node = node.next;
+      l2 = l2.next;
+    }
+  }
+
+  if (!l1) {
+    node.next = l2;
+  } else {
+    node.next = l1;
+  }
+
+  return head;  
 };
 
