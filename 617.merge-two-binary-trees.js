f4c24a1a00287c70c6212ccb2aab7be5b722ad1d
Ivan Kwong <ivan.kwong710@gmail.com>
2019-06-03 16:55:15 -0700

    Clean up solution for mergeTrees

--- 617.merge-two-binary-trees.js
+++ 617.merge-two-binary-trees.js
@@ -15,32 +15,44 @@
  * @param {TreeNode} t2
  * @return {TreeNode}
  */
+// var mergeTrees = function(t1, t2) {
+//   if (!t1) return t2;
+//   if (!t2) return t1;
+
+//   let root = new TreeNode(t1.val + t2.val);
+
+//   if (t1.left && t2.left) {
+//     root.left = mergeTrees(t1.left, t2.left);
+//   }
+
+//   if (t1.right && t2.right) {
+//     root.right = mergeTrees(t1.right, t2.right);
+//   }
+
+//   if (t1.left && !t2.left) {
+//     root.left = t1.left;
+//   } else if (!t1.left && t2.left) {
+//     root.left = t2.left;
+//   }
+
+//   if (t1.right && !t2.right) {
+//     root.right = t1.right;
+//   } else if (!t1.right && t2.right) {
+//     root.right = t2.right;
+//   }
+
+//   return root;  
+// };
+
 var mergeTrees = function(t1, t2) {
+  if (!t1 && !t2) return null;
   if (!t1) return t2;
   if (!t2) return t1;
 
-  let root = new TreeNode(t1.val + t2.val);
+  let newRoot = new TreeNode(t1.val + t2.val);
+  newRoot.left = mergeTrees(t1.left, t2.left);
+  newRoot.right = mergeTrees(t1.right, t2.right);
 
-  if (t1.left && t2.left) {
-    root.left = mergeTrees(t1.left, t2.left);
-  }
-
-  if (t1.right && t2.right) {
-    root.right = mergeTrees(t1.right, t2.right);
-  }
-
-  if (t1.left && !t2.left) {
-    root.left = t1.left;
-  } else if (!t1.left && t2.left) {
-    root.left = t2.left;
-  }
-
-  if (t1.right && !t2.right) {
-    root.right = t1.right;
-  } else if (!t1.right && t2.right) {
-    root.right = t2.right;
-  }
-
-  return root;  
-};
+  return newRoot;
+}
 
