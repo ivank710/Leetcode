c56e376f3ca5da615644ef5638cae35a738ebcc1
Ivan Kwong <ivan.kwong710@gmail.com>
2019-06-06 15:53:03 -0700

    Add tabulation for houseRobber

--- 198.house-robber.js
+++ 198.house-robber.js
@@ -8,11 +8,26 @@
  * @return {number}
  */
 var rob = function(nums, memo = {}) {
-  if (nums.length in memo) return memo[nums.length];
-  if (nums.length === 0) return 0;
+  // if (nums.length in memo) return memo[nums.length];
+  // if (nums.length === 0) return 0;
 
-  let firstEl = nums[0];
-  memo[nums.length] = Math.max(firstEl + rob(nums.slice(2), memo), rob(nums.slice(1), memo));
-  return memo[nums.length];
+  // let firstEl = nums[0];
+  // memo[nums.length] = Math.max(firstEl + rob(nums.slice(2), memo), rob(nums.slice(1), memo));
+  // return memo[nums.length];
+
+  if (nums.length === 0) return 0;
+  if (nums.length === 1) return nums[0];
+
+  let dp = Array(nums.length);
+  dp[0] = nums[0];
+  dp[1] = dp[0] > nums[1] ? dp[0] : nums[1];
+
+  for (let i = 2; i < nums.length; i++) {
+    let val = nums[i];
+
+    dp[i] = dp[i - 1] > val + dp[i - 2] ? dp[i - 1] : val + dp[i - 2]
+  }
+
+  return dp[dp.length - 1]
 };
 
