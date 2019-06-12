46574972b546ed7116d3b20350bef361f0c16064
Ivan Kwong <ivan.kwong710@gmail.com>
2019-06-12 13:52:23 -0700

    Add invertBT

---
+++ 226.invert-binary-tree.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=226 lang=javascript
+ *
+ * [226] Invert Binary Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+  if (!root) return null;
+
+  let treeNode = new TreeNode(root.val);
+  treeNode.left = invertTree(root.right);
+  treeNode.right = invertTree(root.left);
+
+  return treeNode;
+};
+
