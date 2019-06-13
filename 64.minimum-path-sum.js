ada23ddd76e52d25ab59f99620755f92af783352
Ivan Kwong <ivan.kwong710@gmail.com>
2019-06-13 11:19:35 -0700

    Add minPathSum

---
+++ 64.minimum-path-sum.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=64 lang=javascript
+ *
+ * [64] Minimum Path Sum
+ */
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+  //fill first col by summing up vals of going down
+  for (let i = 1; i < grid.length; i++) {
+    grid[i][0] += grid[i - 1][0];
+  } 
+
+  //fill first row by summing up vals of going right
+  for (let i = 1; i < grid[0].length; i++) {
+    grid[0][i] += grid[0][i - 1];
+  }
+
+  for (let i = 1; i < grid.length; i++) {
+    for (let j = 1; j < grid[0].length; j++) {
+      grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+    }
+  }
+
+  return grid[grid.length - 1][grid[0].length - 1];
+};
+
