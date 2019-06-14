c04d85b074291e0092fa40d2b870a59112d86f4d
Ivan Kwong <ivan.kwong710@gmail.com>
2019-06-13 17:40:11 -0700

    Optimize findMin

--- 153.find-minimum-in-rotated-sorted-array.js
+++ 153.find-minimum-in-rotated-sorted-array.js
@@ -7,26 +7,45 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMin = function(nums) {
-  if (nums.length <= 1) return nums[0];
-  let min = nums[0];
+// var findMin = function(nums) {
+//   if (nums.length <= 1) return nums[0];
+//   let min = nums[0];
 
-  let i = 0;
-  let j = nums.length - 1;
+//   let i = 0;
+//   let j = nums.length - 1;
 
-  while (i !== j) {
-    let val1 = nums[i];
-    let val2 = nums[j];
+//   while (i !== j) {
+//     let val1 = nums[i];
+//     let val2 = nums[j];
 
-    if (val1 < val2) {
-      min = Math.min(min, val1)
-      i += 1;
+//     if (val1 < val2) {
+//       min = Math.min(min, val1)
+//       i += 1;
+//     } else {
+//       min = Math.min(min, val2)
+//       j -= 1;
+//     }
+//   }
+
+//   return min;
+// };
+
+const findMin = nums => {
+  if (nums.length === 1) return nums[0];
+
+  let start = 0;
+  let end = nums.length - 1;
+  
+  while (start !== end) {
+    let mid = Math.floor((start + end) / 2);
+    if (nums[start] <= nums[end]) return nums[start];
+    if (nums[mid] < nums[start]) {
+      end = mid;
     } else {
-      min = Math.min(min, val2)
-      j -= 1;
+      start = mid + 1;
     }
   }
 
-  return min;
-};
+  return nums[start];
+}
 
