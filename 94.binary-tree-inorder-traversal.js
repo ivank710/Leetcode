56a876fbcd9f8231361a0002f320e752b5e22f9c
Ivan Kwong <ivan.kwong710@gmail.com>
2019-06-17 15:17:56 -0700

    Add iterativeBTInorder

--- 94.binary-tree-inorder-traversal.js
+++ 94.binary-tree-inorder-traversal.js
@@ -14,24 +14,45 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var inorderTraversal = function(root) {
+// var inorderTraversal = function(root) {
+//   if (!root) return [];
+
+//   let res = [];
+//   helper(root, res);
+
+//   return res;
+// };
+
+// const helper = (root, res) => {
+//   if (root) {
+//     if (root.left) {
+//       helper(root.left, res);
+//     }
+//     res.push(root.val);
+//     if (root.right) {
+//       helper(root.right, res);
+//     }
+//   }
+// };
+
+var inorderTraversal = function (root) {
   if (!root) return [];
 
   let res = [];
-  helper(root, res);
+  let stack = [];
+  let curr = root;
+
+  while (curr || stack.length > 0) {
+    while (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    }
+
+    curr = stack.pop();
+    res.push(curr.val);
+    curr = curr.right;
+  }
 
   return res;
 };
-
-const helper = (root, res) => {
-  if (root) {
-    if (root.left) {
-      helper(root.left, res);
-    }
-    res.push(root.val);
-    if (root.right) {
-      helper(root.right, res)
-    }
-  }
-}
 
