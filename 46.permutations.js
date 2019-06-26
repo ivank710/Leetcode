439f9f0906672dd52d10702923f942e7e1afc22c
Ivan Kwong <ivan.kwong710@gmail.com>
2019-06-26 11:29:49 -0700

    Add backtrack for Permutations

--- 46.permutations.js
+++ 46.permutations.js
@@ -7,19 +7,38 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permute = function(nums) {
-  if (nums.length <= 1) return [nums];
+// var permute = function(nums) {
+//   if (nums.length <= 1) return [nums];
 
-  let last = nums.pop();
-  let prev = permute(nums);
+//   let last = nums.pop();
+//   let prev = permute(nums);
+//   let res = [];
+
+//   prev.forEach(sub => {
+//     for (let i = 0; i <= sub.length; i++) {
+//       res.push(sub.slice(0, i).concat(last).concat(sub.slice(i)));
+//     }
+//   });
+
+//   return res;
+// };
+
+const permute = (nums) => {
   let res = [];
-
-  prev.forEach(sub => {
-    for (let i = 0; i <= sub.length; i++) {
-      res.push(sub.slice(0, i).concat(last).concat(sub.slice(i)));
-    }
-  });
-
+  backtrack(nums, res);
   return res;
+};
+
+const backtrack = (nums, res, n = 0) => {
+  if (n === nums.length - 1) {
+    res.push(nums.slice(0));
+    return;
+  }
+
+  for (let i = n; i < nums.length; i++) {
+    [nums[i], nums[n]] = [nums[n], nums[i]];
+    backtrack(nums, res, n + 1);
+    [nums[i], nums[n]] = [nums[n], nums[i]];
+  }
 };
 
