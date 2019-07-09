108560a278b3106294de19ae8d5de0d4c2d90cc7
Ivan Kwong <ivan.kwong710@gmail.com>
2019-07-08 18:04:27 -0700

    Add uniqueBSTs

---
+++ 96.unique-binary-search-trees.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=96 lang=javascript
+ *
+ * [96] Unique Binary Search Trees
+ */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+  let dp = Array(n + 1).fill(0);
+  dp[0] = 1;
+  
+  for (let i = 1; i <= n; i++) {
+    for(let j = 1; j <= i; j++) {
+      dp[i] += (dp[j - 1] * dp[i - j]);
+    }
+  }
+  
+  return dp[n];
+};
+
